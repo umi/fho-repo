84fe8365b4ad4fb8860a0920670fded74ca4551d
resemara <resemara15644@gmail.com>
2023-10-06 01:02:47 +0900

    マーク検索機能の追加

--- fho/workspace/src/main/java/com/example/demo/repository/SearchRepository.java
+++ fho/workspace/src/main/java/com/example/demo/repository/SearchRepository.java
@@ -2,14 +2,18 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
 import com.example.demo.dto.SearchResultDTO;
 import com.example.demo.entity.Details;
 
-public interface SearchRepository extends Repository<Details, Integer> {
-    @Query("SELECT new com.example.demo.dto.SearchResultDTO(d.id,  f.title, d.time, d.description, f.streamStart) FROM Details d LEFT JOIN Fho f ON d.id = f.id WHERE d.description like CONCAT('%', :description, '%') and f.title like CONCAT('%', :title, '%') ")
-    public List<SearchResultDTO> searchDetails(@Param("description") String description, @Param("title") String title);
+public interface SearchRepository extends JpaRepository<Details, Integer> {
+    @Query("SELECT new com.example.demo.dto.SearchResultDTO(d.id,  f.title, d.time, d.description, f.streamStart, m.mark) "
+    		+ "FROM Details d LEFT JOIN Fho f ON d.id = f.id "
+    		+ "LEFT JOIN StreamMark sm ON d.streamId = sm.streamId "
+    		+ "LEFT JOIN Mark m ON sm.markId = m.markId "
+    		+ "WHERE d.description LIKE CONCAT('%', :description, '%') AND f.title LIKE CONCAT('%', :title, '%') AND sm.markId = :markId ")
+    public List<SearchResultDTO> searchDetails(@Param("description") String description, @Param("title") String title,  @Param("markId") int markId);
 }
