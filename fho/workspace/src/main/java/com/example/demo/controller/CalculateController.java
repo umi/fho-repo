4b54e62c8b3e0af4e76b85ad4ffe8700ecb62c8b
resemara <resemara15644@gmail.com>
2023-10-16 02:30:29 +0900

    配信情報ページの追加

--- fho/workspace/src/main/java/com/example/demo/controller/CalculateController.java
+++ fho/workspace/src/main/java/com/example/demo/controller/CalculateController.java
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.dto.BattleDTO;
 import com.example.demo.dto.CalculateResultDTO;
+import com.example.demo.entity.Fho;
 import com.example.demo.service.CalculateService;
+import com.example.demo.service.FhoService;
+import com.example.demo.util.SecondsToTimeConverter;
 
 @Controller
 @RequestMapping("/calculate")
@@ -19,17 +22,46 @@ public class CalculateController {
 	@Autowired
 	private CalculateService calculateService;
 	
+	@Autowired
+	private FhoService fhoService;
+	
 	 @GetMapping
 	    public String index(Model model) {
+		 
+	        return "calculate/index";
+	    }
+	 
+	 @GetMapping("/user")
+	    public String user(Model model) {
 		 
 		 List<BattleDTO> battles = calculateService.clculateBattle();
 		 
 		    List<CalculateResultDTO> calcs = battles.stream()
-                    .map(CalculateResultDTO::fromBattleDTO)
-                    .collect(Collectors.toList());
+                 .map(CalculateResultDTO::fromBattleDTO)
+                 .collect(Collectors.toList());
 		 
 	        model.addAttribute("calc", calcs);
-	        return "calculate/index";
+	        return "calculate/user";
 	    }
 	 
+	 @GetMapping("/fho")
+	    public String fho(Model model) {
+		 
+		 List<Fho> fhoes = fhoService.getFho();
+		 
+		 int totalTime = 0;
+		 int count = 0;
+		 String tTime;
+		 
+		 for(Fho fho: fhoes) {
+			 totalTime += fho.getTotal();
+			 count++;
+		 }
+		 tTime = SecondsToTimeConverter.convertToTime(totalTime);
+		 
+	        model.addAttribute("tTime", tTime);
+	        model.addAttribute("count", count);
+	        
+	        return "calculate/fho";
+	    }
 }
