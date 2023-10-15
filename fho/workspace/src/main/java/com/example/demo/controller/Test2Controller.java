fdcc9b6b1fa16fe7318f8a1c0555bd3762470a7e
umi <umihara666@gmail.com>
2023-10-15 19:40:33 +0200

    test

--- fho/workspace/src/main/java/com/example/demo/controller/Test2Controller.java
+++ fho/workspace/src/main/java/com/example/demo/controller/Test2Controller.java
@@ -21,11 +21,17 @@ public class Test2Controller {
 		Pattern headPattern = DocumentParser.getHeadLinePattern();
 
 		// Matcher matcherHead = headPattern.matcher("9/23    11:18:23    練習や、マジ(10:56～)");
-		Matcher matcherHead = headPattern.matcher("3:10:44    今日何時間やんねん、マジ(22:16～)");
+		// Matcher matcherHead = headPattern.matcher("3:10:44    今日何時間やんねん、マジ(22:16～)");
 		// Matcher matcherHead = headPattern.matcher("  0:18～あーしんど");
+		Matcher matcherHead = headPattern.matcher("①10/1    3:30:12    夜中(2:13～)");
 
 		if(matcherHead.matches()){
 			content.add(matcherHead.group());
+
+			content.add(matcherHead.group(1));
+			content.add(matcherHead.group(2));
+			content.add(matcherHead.group(3));
+			content.add(matcherHead.group(4));
 		}
 
         model.addAttribute("content", content);
